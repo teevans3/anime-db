e3cd5eb69911fc3d21cd38b9532a73914fb80d06
Thomas Evans <thomasevans@Thomass-MBP.attlocal.net>
2021-07-22 21:30:48 -0400

    changed header component to be a link; styled header and 'about' section

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import Link from 'next/link';
 import {Grid, Typography} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    Header: {
+        fontSize: '4rem',
+        padding: '1rem 0',
+        textAlign: 'center'
+    }
+})
 
 const Header = (props) => {
+    const classes = useStyles();
+    // trying to get Header to link home
     return (
         <Grid container>
-            <Typography variant="h2" align="center" gutterBottom >Anime Database</Typography>
+            <Grid xs={12} className={classes.Header}>
+                <Link href="/" passHref >
+                    Anime Database
+                </Link>
+            </Grid>
             {/* add pseudo-"about" section if on home page */}
             {props.home ? 
                 <Grid container>
                     <Grid item xs={2} />
                     <Grid item xs={8}>
-                        <Typography variant="subtitle1">
+                        <Typography variant="h6" align="justify">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </Typography>
                     </Grid>
